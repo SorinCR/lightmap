2e9496c97f288fca1a81a42a983819b1b6314a63
Cruceru Sorin <sorin@Sorin.e-uvt.ro>
2024-03-09 16:40:46 +0200

    map events w. bounds + overpass api

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,15 +13,52 @@ import {
   useMapEvents,
 } from "react-leaflet";
 
-export default function Map() {
-  //   useMapEvents({
-  //     zoomend(e) {
-  //       console.log(e);
-  //     },
-  //   });
+import L from "leaflet";
 
+function MapEvents() {
+  const map = useMap();
+
+  function processIntersectionData(data) {
+    const intersections = data.elements;
+
+    for (let intersection of intersections) {
+      if (intersection.type === "node") {
+        L.marker([intersection.lat, intersection.lon])
+          .addTo(map) // Replace 'yourMap' with your Leaflet map object
+          .bindPopup("Intersection");
+      }
+    }
+  }
+
+  useMapEvents({
+    zoomend(e) {
+      var zoom = e.target._zoom;
+      if (zoom >= 18) {
+        console.log(e);
+        const bounds = map.getBounds();
+        console.log();
+
+        const overpassQuery = `[out:json][timeout:25];
+        (
+            way["highway"](${bounds._southWest.lat},${bounds._southWest.lng},${bounds._northEast.lat},${bounds._northEast.lng}); 
+        );
+        out body;
+        >;
+        out skel qt;`;
+
+        console.log(overpassQuery);
+      }
+    },
+    // click(e) {
+    //   console.log(e);
+    // },
+  });
+  return null;
+}
+
+export default function Map() {
   return (
-    <div className="w-full h-full flex">
+    <div className="w-full h-full">
       <MapContainer
         center={[45.7494, 21.2272]}
         zoom={15}
@@ -35,6 +72,7 @@ export default function Map() {
         ]}
         doubleClickZoom={false}
       >
+        <MapEvents />
         <TileLayer
           attribution="Google Maps"
           url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
